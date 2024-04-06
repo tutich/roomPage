ce58a868261e51ab5ee280592bdd18846789bd0a
tutich <pkoinange02@gmail.com>
2024-04-06 22:53:17 +0300

    about css, responsiveness

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -5,21 +5,21 @@ import light from '../../images/image-about-light.jpg'
 const About = () => {
   return (
     <div>
-        <section>
-            <div>
-                <img src={dark} alt="" />
+        <section className='flex flex-col lg:flex-row '>
+            <div >
+                <img src={dark} alt=""  className='w-full '/>
             </div>
             
-            <div>
-                <h1>ABOUT OUR FURNITURE</h1>
-                <p>Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a 
+            <div className='max-w-md w-[90%] mx-auto text-left '>
+                <h3 className='tracking-wide leading-relaxed text-l'>ABOUT OUR FURNITURE</h3>
+                <p className='text-[##A1A1A1] leading-relaxed mx-auto'>Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a 
                     cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from 
                     traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.
                 </p>
             </div>
 
             <div>
-                <img src={light} alt="" />
+                <img src={light} alt="" className='w-full '/>
             </div>
         </section>
     </div>
