4f7f736bdd6e2a500de09140442b76da16bc8923
tutich <pkoinange02@gmail.com>
2024-04-07 16:26:30 +0300

    web responsive, nav responsive

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className='absolute z-10 p-8 flex justify-center items-center w-full lg:w-auto lg:flex-start '>
+    <div className='absolute z-10 p-8 flex justify-center items-center w-full lg:w-auto lg:items-start lg:justify-start '>
       <div className='flex justify-between' >
         <div className='absolute block lg:hidden z-20  left-6 items-center'> 
           {
@@ -26,12 +26,13 @@ const Navbar = () => {
           <img src={logo} alt="" className='' />
         </div>
       </div>
-      <nav className={`bg-white absolute top-0 left-0 w-full py-8 lg:py-0 lg:w-auto shadow-lg ${isOpen ? '' : 'hidden'} `}  >
-        <ul className='flex justify-center items-center gap-8'>
-          <li className='cursor-pointer border-b-4 border-transparent hover:border-neutral-900 transition-all durarion-500'>home</li>
-          <li className='cursor-pointer border-b-4 border-transparent hover:border-neutral-900 transition-all durarion-500'>shop</li>
-          <li className='cursor-pointer border-b-4 border-transparent hover:border-neutral-900 transition-all durarion-500'>about</li>
-          <li className='cursor-pointer border-b-4 border-transparent hover:border-neutral-900 transition-all durarion-500'>contact</li>
+  
+      <nav className={` absolute top-0 left-0 w-full py-8  lg:w-auto  ${isOpen ? '' : 'hidden lg:block'} ${isOpen ? 'bg-white shadow-lg' : ''}`}  >
+        <ul className='flex justify-center lg:justify-start lg:gap-14 items-center gap-8 lg:px-40 -mt-1.5'>
+          <li className='lg:text-white text-xl cursor-pointer border-b-4 border-transparent hover:border-neutral-900 transition-all durarion-500'>home</li>
+          <li className='lg:text-white text-xl cursor-pointer border-b-4 border-transparent hover:border-neutral-900 transition-all durarion-500'>shop</li>
+          <li className='lg:text-white text-xl cursor-pointer border-b-4 border-transparent hover:border-neutral-900 transition-all durarion-500'>about</li>
+          <li className='lg:text-white text-xl cursor-pointer border-b-4 border-transparent hover:border-neutral-900 transition-all durarion-500'>contact</li>
         </ul>
       </nav>
     </div>
